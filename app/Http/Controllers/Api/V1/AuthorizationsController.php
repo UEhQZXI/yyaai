1af2d8674123a170f4ef81437bc559a67b0fc9ef
lutianxiong <tianxionglu@gmail.com>
2018-08-28 16:32:15 +0800

    添加登录方法

---
+++ app/Http/Controllers/Api/V1/AuthorizationsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\Api\V1\AuthorizationRequest;
+use Illuminate\Http\Request;
+
+class AuthorizationsController extends Controller
+{
+    public function store(AuthorizationRequest $request)
+    {
+        $phone = $request->phone;
+        $password = $request->password;
+
+        $loginInfo['phone'] = $phone;
+        $loginInfo['password'] = $password;
+
+        if (!$token = \Auth::guard('api')->attempt($loginInfo)) {
+            return $this->response->errorUnauthorized('账号或密码错误');
+        }
+
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ])->setStatusCode(201);
+    }
+}
---
+++ app/Http/Requests/Api/V1/AuthorizationRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Dingo\Api\Http\FormRequest;
+
+class AuthorizationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'phone' => 'required',
+            'password' => 'required|string|min:6',
+        ];
+    }
+}
