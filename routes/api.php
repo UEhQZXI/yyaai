7c1d225a6d68920859b16a8ec188303575ab0d97
lutianxiong <tianxionglu@gmail.com>
2018-08-28 14:47:12 +0800

    添加接口节流限制

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,16 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api\V1'
 ], function($api) {
-    // 发送短信验证码
-    $api->post('verificationCodes','VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
 
-    // 用户注册
-    $api->post('users','UsersController@store')
-        ->name('api.users.store');
+    // 接口节流处理
+    $api->group(['middleware' =>  'api.throttle', 'limit' => config('api.rate_limits.sign.limit'), 'expires' => config('api.rate_limits.sign.expires'),], function ($api) {
+        // 发送短信验证码
+        $api->post('verificationCodes','VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+
+        // 用户注册
+        $api->post('users','UsersController@store')
+            ->name('api.users.store');
+    });
+
 });
