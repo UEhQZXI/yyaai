bc0c56037c294691dfaed02e83f59d0662376374
lutianxiong <tianxionglu@gmail.com>
2018-11-21 11:39:31 +0800

    fix

--- public/js/wap/about.js
+++ public/js/wap/about.js
@@ -5,12 +5,13 @@ $(function(){
         mui.confirm("确认退出",function(e){
             console.log(e)
             if(e.index == 1){
-               location.href = "/my/needLogin"
-               localStorage.removeItem("token")
-               localStorage.removeItem("name")
+               location.href = "/my"
+               localStorage.removeItem("token");
+               localStorage.removeItem("name");
+               localStorage.removeItem("avatar");
             }
         })
    })
 
 
-})
+});
