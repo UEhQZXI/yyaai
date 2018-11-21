bee4ca995575d2dcd135811d1d29fbf76e9d67e4
lutianxiong <tianxionglu@gmail.com>
2018-11-21 11:10:47 +0800

    update verfication

--- app/Http/Requests/Api/V1/VerificationCodeRequest.php
+++ app/Http/Requests/Api/V1/VerificationCodeRequest.php
@@ -33,10 +33,9 @@ class VerificationCodeRequest extends FormRequest
                 break;
             case 'bindPhone':
                 return $rule = [
-                    'phone' => [
-                        'required',
-                        'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/'
-                    ]];
+                    'verification_key' => 'required',
+                    'verification_code' => 'required'
+                ];
                 break;
             default:
                 return $rule = [
